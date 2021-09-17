f814235fdd21aac985e0c4d0482a66be115e8964
arunsammit <arunsammit@gmail.com>
2021-09-17 19:46:13 +0530

    fixed client and server code

--- PipelinedClient.java
+++ PipelinedClient.java
@@ -3,7 +3,6 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.util.TimerTask;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -13,8 +12,6 @@ public class PipelinedClient {
     private final DatagramPacket[] sendPackets;
     // buffer length
     private final int N;
-    // max sequence Number
-    private final int maxSeqNum;
     // sequence number of the oldest unacknowledged packet
     private int base;
     // sequence number of the next packet to be sent
@@ -26,86 +23,88 @@ public class PipelinedClient {
     // datagramSocket for communication with server
     private final DatagramSocket socket;
     // variable to indicate that client is still waiting for acknowledgement packets from server
-    private boolean listening;
+    private boolean endSending;
     // executorService for scheduling timeout events
     ScheduledExecutorService scheduledService;
     // ScheduledFuture representing the future scheduled task
     ScheduledFuture<?> scheduledFuture;
 
-    private long delay;
+    private final long delay;
     //task to be executed in each timeout event
-    public PipelinedClient(int bufferLength, InetAddress address, int port) throws SocketException {
+    public PipelinedClient(int bufferLength, InetAddress address, int port, long delay) throws SocketException {
         this.sendPackets = new DatagramPacket[bufferLength];
         N = bufferLength;
-        maxSeqNum = bufferLength + 1;
-        base = 0;
-        nextSeqNum = 0;
+        base = 1;
+        nextSeqNum = 1;
         this.address = address;
         this.port = port;
-        this.listening = true;
         socket = new DatagramSocket();
-        delay = 100;
+        this.delay = delay;
         scheduledService = Executors.newSingleThreadScheduledExecutor();
         Runnable r = this::receivePackets;
-        new Thread(r).start();
+        new Thread(r,"receiving").start();
     }
 
     private void receivePackets() {
         try {
             byte[] receiveBuf = new byte[1024];
             DatagramPacket receivePacket = new DatagramPacket(receiveBuf, receiveBuf.length);
-            while (listening) {
+            while (true) {
                 socket.receive(receivePacket);
                 synchronized (this){
                     var seqNumber = PacketUtils.extractPacket(receivePacket).getSeqNum();
-                    System.out.println("received acknowledgement for packet number " + seqNumber);
-                    System.out.println("incrementing base to " + seqNumber + 1);
-                    base = (seqNumber + 1) % maxSeqNum;
+                    System.out.print("received ack for packet  " + seqNumber);
+                    base = seqNumber + 1;
+                    System.out.println(", setting base = " + base);
+                    this.notify();
                     scheduledFuture.cancel(true);
                     if(base != nextSeqNum){
-                        scheduledFuture = scheduledService.schedule(this::scheduledTask, delay, TimeUnit.MILLISECONDS);
+                        scheduledFuture = scheduledService.schedule(this::retransmitPackets, delay, TimeUnit.MILLISECONDS);
+                    } else if (endSending){
+                        System.out.println("Stopping receiving packets since endSending flag is true");
+                        socket.close();
+                        scheduledService.shutdown();
+                        break;
                     }
                 }
             }
-            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void scheduledTask() {
-        System.out.println("Timeout occurred !");
-        scheduledFuture = scheduledService.schedule(this::scheduledTask, delay, TimeUnit.MILLISECONDS);
-        for (int i = base; i != nextSeqNum; i = (i+1)%maxSeqNum) {
+    private synchronized void retransmitPackets() {
+
+        System.out.println("Timeout occurred ! base is " + base + " nextSeqNum is " + nextSeqNum);
+        scheduledFuture = scheduledService.schedule(this::retransmitPackets, delay, TimeUnit.MILLISECONDS);
+        for (int i = base; i < nextSeqNum; i = i + 1) {
             System.out.println("retransmitting packet number: " + i);
             try{
-                socket.send(sendPackets[i]);
+                socket.send(sendPackets[i%N]);
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
     }
-    
 
-    public synchronized boolean canSend(){
-        return ( nextSeqNum + 1 ) % maxSeqNum != base;
+    public void stopTransmission() {
+        System.out.println("Stop Transmission called . . .");
+        endSending = true;
     }
-    public void stop() {
-        listening = false;
-    }
-    public synchronized void sendData(String data) throws IOException {
-        if(canSend()){
-            var packet = PacketUtils.createPacket(address, port, nextSeqNum, data);
-            sendPackets[nextSeqNum % N] = packet;
-            System.out.println("sending the packet with sequence number " + nextSeqNum);
-            socket.send(packet);
-            if(base == nextSeqNum){
-                scheduledFuture = scheduledService.schedule(this::scheduledTask, delay, TimeUnit.MILLISECONDS);
-            }
-            nextSeqNum = ( nextSeqNum + 1 ) % maxSeqNum;
-            return;
+    public synchronized void sendData(String data) throws IOException, InterruptedException {
+        var packet = PacketUtils.createPacket(address, port, nextSeqNum, data);
+        while (nextSeqNum >= base + N) {
+            System.out.println("buffer full, waiting for ack of sent packet : " + base);
+            this.wait();
         }
-        throw new RuntimeException("Buffer Capacity Full");
+        sendPackets[nextSeqNum % N] = packet;
+        System.out.println("sending packet: " + nextSeqNum);
+        socket.send(packet);
+        if(base == nextSeqNum){
+            scheduledFuture = scheduledService.schedule(this::retransmitPackets, delay, TimeUnit.MILLISECONDS);
+        }
+        nextSeqNum = nextSeqNum + 1 ;
+        System.out.println("nextSeqNum = " + nextSeqNum);
     }
 
 }
--- Server.java
+++ Server.java
@@ -3,25 +3,27 @@ import java.io.*;
 
 public class Server {
     public static void main(String[] args) throws IOException, InterruptedException {
-        if (args.length != 4) {
-            System.err.println("Usage: java Server <port number> <probability of error> <probability of dropping> <channel delay (ms)> <buffer Length> ");
+        if (args.length != 5) {
+            System.err.println("Usage: java Server <port number> <probability of error> <probability of dropping> <channel delay (ms)> <output file path>");
             System.exit(1);
         }
         int portNumber = Integer.parseInt(args[0]);
         double probabilityOfError = Double.parseDouble(args[1]);
-        double probabilityOfDrop = Integer.parseInt(args[2]);
+        double probabilityOfDrop = Double.parseDouble(args[2]);
         long channeldelay = Long.parseLong(args[3]);
-        int bufferLength = Integer.parseInt(args[4]);
-        int maxSeqNum = bufferLength + 1;
         int expectedSeqNum =  1;
-        boolean listening = true;
         System.out.println("Server started ...");
-        try (DatagramSocket socket = new DatagramSocket(portNumber)) {
-            while (listening) {
+        File file = new File(args[4]);
+        file.createNewFile();
+        File logFile = new File("Server.log");
+        PrintStream stream = new PrintStream(logFile);
+        System.setOut(stream);
+        try(FileWriter outputWriter = new FileWriter(file);
+            DatagramSocket socket = new DatagramSocket(portNumber)){
+            while (true) {
                 byte[] buf = new byte[1024];
                 // packet for receiving the message
                 DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                System.out.println("Waiting for client ...");
                 // message from client
                 socket.receive(packet);
                 // simulating channel delay
@@ -37,24 +39,25 @@ public class Server {
                     int seqNum = info.getSeqNum();
                     String received = info.getMessage();
                     if(seqNum != expectedSeqNum){
-                        System.out.println("Out of order packet received! Hence dropping");
-                        continue;
+                        System.out.println("Out of order packet " + seqNum + " !");
+                    } else {
+                        expectedSeqNum ++;
+                        System.out.println("Received message number " + seqNum);
+                        outputWriter.write(seqNum + " " + received + "\n");
+                        outputWriter.flush();
                     }
-                    expectedSeqNum = (expectedSeqNum + 1) % maxSeqNum;
-                    // expectedSeqNum ++;
-                    System.out.println("Received message number " + seqNum + ":\n" + received);
-                    
                 }
                 InetAddress address = packet.getAddress();
                 int port = packet.getPort();
-                int previousSeqNum =  (expectedSeqNum - 1 + maxSeqNum) % maxSeqNum;
-                // int previousSeqNum = expectedSeqNum - 1;
+                int previousSeqNum = expectedSeqNum - 1;
                 DatagramPacket sendPacket = PacketUtils.createPacket(address, port, previousSeqNum, "ACK");
                 choiceOfDrop = Math.random();
                 if(choiceOfDrop < probabilityOfDrop) continue;
+                System.out.println("Sending ack for " + previousSeqNum);
                 socket.send(sendPacket);
             }
         }
+        
     }
 
 }
